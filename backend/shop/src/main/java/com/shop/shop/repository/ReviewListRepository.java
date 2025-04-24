1677563cbcedbff540b104f7b1f3d30f85d91cb5
ITSC <itsc@example.com>
2025-04-24 12:36:27 +0900

    리뷰 리스트 ItemId를 기준으로 페이징 조회 기능 구현 완료

--- backend/shop/src/main/java/com/shop/shop/repository/ReviewListRepository.java
+++ backend/shop/src/main/java/com/shop/shop/repository/ReviewListRepository.java
@@ -15,6 +15,16 @@ public interface ReviewListRepository extends JpaRepository<ReviewList, Long> {
     @Query("SELECT rl FROM ReviewList rl WHERE rl.id = :reviewId")
     public ReviewList findByIdWithReviewImages(@Param("reviewId") Long reviewId);
 
+    // 특정 상품Id를 기준으로 모든 리뷰글을 이미지와 함께 조회(페이징) 삭제(delFlag = true) 포함
+    @EntityGraph(attributePaths = "images")
+    @Query("SELECT rl FROM ReviewList rl WHERE rl.item.id = :itemId")
+    public Page<ReviewList> findAllByItemIdWithReviewImages(@Param("itemId") Long itemId, Pageable pageable);
+
+    // 특정 상품Id를 기준으로 모든 리뷰글을 이미지와 함께(페이징) 조회 삭제(delFlag = true) 미포함
+    @EntityGraph(attributePaths = "images")
+    @Query("SELECT rl FROM ReviewList rl WHERE rl.item.id = :itemId AND rl.delFlag = false")
+    public Page<ReviewList> findAllByItemIdWithReviewImagesANDDelFlag(@Param("itemId") Long itemId, Pageable pageable);
+
     // 모든 리뷰글을 이미지와 함께 조회(페이징) 삭제(delFlag = true) 포함
     @EntityGraph(attributePaths = "images")
     @Query("SELECT rl FROM ReviewList rl ORDER BY rl.id DESC")
--- backend/shop/src/main/java/com/shop/shop/service/ReviewListService.java
+++ backend/shop/src/main/java/com/shop/shop/service/ReviewListService.java
@@ -14,6 +14,8 @@ public interface ReviewListService {
     public ReviewListDTO getReviewListWithReviewImages(Long reviewListId);
     public Page<ReviewListDTO> getReviewListPage(Pageable pageable);
     public Page<ReviewListDTO> getReviewListPageWithDelFlag(Pageable pageable);
+    public Page<ReviewListDTO> getReviewListPageByItemId(Long itemId, Pageable pageable);
+    public Page<ReviewListDTO> getReviewListPageByItemIdWithDelFlag(Long itemId, Pageable pageable);
     public ReviewListDTO editReviewList(ReviewListDTO reviewListDTO, List<MultipartFile> files);
     public void deleteReviewList(ReviewListDTO reviewListDTO);
 
